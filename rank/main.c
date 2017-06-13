f2ac25fd6fd6ff7c42e3c3f9463b82e7093510cb
20161104567 <846129391@qq.com>
2017-06-13 11:35:33 +0800

    排序
    十个整数排序

--- rank/main.c
+++ rank/main.c
@@ -8,8 +8,31 @@
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main()
+{
+    int i,j,a;
+    int f[10];
+    printf("输入要排序的十个数字:\n");
+    for(i=0;i<10;i++)
+    {
+        scanf("%d",&f[i]);
+    }
+    for(i=0;i<9;i++)
+    {
+        for(j=i+1;j<10;j++)
+        {
+            if(f[i]>f[j])
+            {
+                a=f[i];
+                f[i]=f[j];
+                f[j]=a;
+            }
+        }
+    }
+    printf("排序为：");
+    for(i=0;i<10;i++)
+    {
+        printf("%d ",f[i]);
+    }
     return 0;
 }
